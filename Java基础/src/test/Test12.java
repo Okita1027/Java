426173336ba92f6fdfd4b036df4eeb83c26833bb
qzy1027 <okita1027@outlook.com>
2023-01-06 22:23:53 +0800

    Java程序设计（基础篇）原书第10版-梁勇

--- Java基础/src/test/Test12.java
+++ Java基础/src/test/Test12.java
@@ -2,6 +2,12 @@ package test;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * @Author qzy
  * @ClassName Test12
@@ -9,7 +15,6 @@ import org.junit.jupiter.api.Test;
  * @Date 2023/1/4 21:33 星期三
  */
 public class Test12 {
-
     @Test
     public void test01() {
         try {
@@ -56,4 +61,89 @@ public class Test12 {
 
         }
     }
+
+    @Test
+    void test03() {
+        try {
+            System.out.println(1 / 0);
+        } catch (Exception e) {
+            /*
+                Java在处理表达式计算时，默认对整数型0转型成0.0，而浮点数在计算机内是无法绝对精确的表示的，
+                因此这个0.0在计算机内部只是一个接近0的极小浮点值而已，所以很容易的可以想出为什么输出Infinity。
+                同样的取模运算%也存在这个问题。
+             */
+            System.out.println(1.0 / 0);
+        }
+        long value = Long.MAX_VALUE + 1;
+        System.out.println(value);//溢出-9223372036854775808
+    }
+
+    @Test
+    void test04() {
+        try {
+            String s = "abc";
+            System.out.println(s.charAt(3));
+        } catch (Exception exception) {
+            System.out.println("exception.toString() = " + exception);
+            System.out.println("...");
+        }
+        System.out.println("Test12.test04");
+        try {
+            Object o = new Object();
+            String d = (String) o;
+        } catch (Exception e) {
+            System.out.println("e = " + e);
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    void testFile() throws IOException {
+        File fire = new File("src/test/fire.txt");
+        if (!fire.exists()) {
+            fire.createNewFile();
+            PrintWriter printWriter = new PrintWriter(fire);
+            printWriter.write("hhh");
+            printWriter.println(123);
+            printWriter.close();
+        }
+        //try with resource -> 自动关闭资源
+        try (BufferedReader reader = new BufferedReader(new FileReader(fire))) {
+            String str;
+            while ((str = reader.readLine()) != null) {
+                System.out.println("str = " + str);
+            }
+        }
+
+        fire.delete();
+    }
+
+    @Test
+    void test05() {
+        System.out.print("---");
+        System.out.printf("%5s", true);
+        System.out.println("---");
+    }
+
+    @Test
+    void testURL() throws IOException {
+        URL url = new URL("https://www.baidu.com");
+        InputStream inputStream = url.openStream();
+        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream)) {
+            byte[] bytes = bufferedInputStream.readAllBytes();
+            System.out.println("bytes = " + bytes.length);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        double d = scanner.nextDouble();
+        String s = scanner.nextLine();
+        System.out.print(i);
+        System.out.print(d);
+        System.out.print(s);
+    }
+
 }
---
+++ Java基础/src/test/Test13.java
@@ -0,0 +1,51 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * @Author qzy
+ * @ClassName Test13
+ * @Description TODO(一句话描述该类功能)
+ * @Date 2023/1/6 19:09 星期五
+ */
+public class Test13 {
+    class A implements Cloneable {
+        B b;
+        C c;
+        public A() {
+            b = new B();
+            c = new C();
+        }
+        @Override
+        protected A clone() throws CloneNotSupportedException {
+            A clone = (A) super.clone();
+            clone.b = b.clone();
+            return clone;
+        }
+    }
+
+    class B implements Cloneable {
+        @Override
+        protected B clone() throws CloneNotSupportedException {
+            return (B) super.clone();
+        }
+    }
+
+    class C {
+
+    }
+
+    @Test
+    void test01() throws CloneNotSupportedException {
+        A a = new A();
+        A clone = a.clone();
+        System.out.println(a == clone);//false
+        System.out.println(a.b == clone.b);//false
+        System.out.println(a.c == clone.c);//true
+    }
+
+
+}
